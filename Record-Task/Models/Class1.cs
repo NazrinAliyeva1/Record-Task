2883a6f175f79e8093d1c162e983e7f42625f9f1
NazrinAli1 <anazrin711@gmail.com>
2024-04-05 08:34:31 -0600

    created record

---
+++ Record-Task/Models/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Record_Task.Models
+{
+    internal record Class1
+    {
+        public string Name="XXX";
+        public string Surname="YYY";
+        public byte Age=0;
+        public string Job="WWW";
+
+
+        public Class1(string name, string surname, byte age, string job)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Job = job;
+
+        }
+    }
+}
---
+++ Record-Task/Program.cs
@@ -0,0 +1,19 @@
+﻿using Record_Task.Models;
+using System.Xml.Linq;
+
+namespace Record_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 std = new Class1("Nazrin", "Aliyeva", 20, "BackEnd Developer.");
+            Class1 std2 = std with { Name = "Sefa", Surname = "Mikayilova", Age = 54, Job = "Doctor" };
+            Console.WriteLine(std);
+            Console.WriteLine(std2);
+            //Class1 std2 = new Class1("A", "B");
+            Console.WriteLine(Object.ReferenceEquals(std, std2));
+            Console.WriteLine(std==std2);
+        }
+    }
+}
